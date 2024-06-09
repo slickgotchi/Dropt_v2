c6624d9fe6077b9effd837a7b7ff2383dae5b5dd
Slick <slickgotchi@gmail.com>
2024-06-09 17:20:44 +0800

    adding AI

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(new Vector3(0, 0, 0));
+    }
+}
