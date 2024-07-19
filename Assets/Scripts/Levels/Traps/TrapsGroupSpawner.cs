5e0d9aac974c6b38fb02b841907b97758a9fea17
Nikita < >
2024-07-19 16:43:49 +0300

    [ADD] Client/Server interaction

--- Assets/Scripts/Levels/Traps/TrapsGroupSpawner.cs
+++ Assets/Scripts/Levels/Traps/TrapsGroupSpawner.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Level.Traps
 {
     public sealed class TrapsGroupSpawner : MonoBehaviour
     {
         public TrapSpawner[] Spawners;
-
-        public int MaxGroupsCount { get; private set; }
-
-        public void AddChild(int group)
-        {
-            MaxGroupsCount = Math.Max(group + 1, MaxGroupsCount);
-        }
     }
 }
