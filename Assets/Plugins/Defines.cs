f6038d1dff8d7da800e0bc87b6d86dbf2bba3a05
Nikita < >
2024-07-24 16:51:12 +0300

    [ADD] player prediction with traps

---
+++ Assets/Plugins/Defines.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Plugins
+{
+    public static class Defines
+    {
+        public static bool FAST_START
+        {
+            get
+            {
+                bool result = false;
+#if UNITY_EDITOR
+                string path = Application.dataPath + "/../Library/";
+                result = System.IO.File.Exists(path + "FAST_START");
+#endif
+
+                return result;
+            }
+        }
+    }
+}
