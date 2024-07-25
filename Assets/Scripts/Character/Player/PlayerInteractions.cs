dedfa418742c01cba616a9ae7fb173a51f59a7c1
Slick <slickgotchi@gmail.com>
2024-07-25 12:15:14 +0800

    tweaking thirdweb

--- Assets/Scripts/Character/Player/PlayerInteractions.cs
+++ Assets/Scripts/Character/Player/PlayerInteractions.cs
@@ -56,60 +56,8 @@ public class PlayerInteractions : NetworkBehaviour
         }
 
         m_interactable.OnUpdateInteraction();
-
-
-        //m_nextLevelCooldownTimer -= Time.deltaTime;
-
-        //// reset all UI canvas
-        //ResetUICanvii();
-
-        //// test for any interaction hits
-        //var interactionHit = Physics2D.OverlapCircle(
-        //    transform.position + new Vector3(0, 0.6f, 0),
-        //    0.5f,
-        //    m_interactablesLayer);
-        //if (interactionHit == null)
-        //{
-        //    m_interactableObject = null;
-        //    return;
-        //}
-
-        //// get interactable object
-        //m_interactableObject = interactionHit.gameObject;
-
-        //// show UI based on interaction type
-        //if (IsLocalPlayer)
-        //{
-        //    // reset f hold timer if no f down
-        //    if (!Input.GetKey(KeyCode.F)) m_fHoldTimer = 0;
-
-        //    HandleLocalHoleInteractions();
-        //    HandleLocalWeaponSwapInteractions();
-        //    HandleLocalEscapePortalInteractions();
-        //    HandleLocalGotchiSelectPortalInteractions();
-        //}
-
-        //// set interactable fill bar
-        //InteractableUICanvas.Instance.InteractSlider.value = m_fHoldTimer / k_fHoldtime;
     }
 
-    private void HandleLocalHoleInteractions()
-    {
-        if (m_interactableObject.HasComponent<Hole>())
-        {
-            InteractableUICanvas.Instance.InteractTextbox.SetActive(true);
-
-            if (Input.GetKey(KeyCode.F))
-            {
-                m_fHoldTimer += Time.deltaTime;
-                if (m_fHoldTimer >= k_fHoldtime && m_nextLevelCooldownTimer <= 0)
-                {
-                    TryGoToNextLevelServerRpc();
-                    m_nextLevelCooldownTimer = k_nextLevelCooldown;
-                }
-            }
-        }
-    }
 
     private void HandleLocalEscapePortalInteractions()
     {
@@ -128,21 +76,6 @@ public class PlayerInteractions : NetworkBehaviour
             }
         }
     }
-
-    //private void HandleLocalGotchiSelectPortalInteractions()
-    //{
-
-    //    if (m_interactableObject.HasComponent<GotchiSelectPortal>())
-    //    {
-    //        InteractableUICanvas.Instance.InteractTextbox.SetActive(true);
-
-    //        if (Input.GetKey(KeyCode.F))
-    //        {
-    //            ThirdwebCanvas.Instance.Container.SetActive(true);
-    //            GotchiSelectCanvas.Instance.Container.SetActive(true);
-    //        }
-    //    }
-    //}
 
     private void HandleLocalWeaponSwapInteractions()
     {
@@ -165,25 +98,6 @@ public class PlayerInteractions : NetworkBehaviour
                 m_interactableObject.GetComponent<WeaponSwap>().Init(ogEquipment);
             }
         }
-    }
-
-    //private void ResetUICanvii()
-    //{
-    //    InteractableUICanvas.Instance.InteractTextbox.SetActive(false);
-    //    WeaponSwapCanvas.Instance.Container.SetActive(false);
-    //    ThirdwebCanvas.Instance.Container.SetActive(false);
-    //    GotchiSelectCanvas.Instance.Container.SetActive(false);
-    //}
-
-    [Rpc(SendTo.Server)]
-    void TryGoToNextLevelServerRpc()
-    {
-        if (m_interactableObject == null) return;
-        if (!m_interactableObject.HasComponent<Hole>()) return;
-        if (m_nextLevelCooldownTimer > 0) return;
-
-        LevelManager.Instance.GoToNextLevel();
-        m_nextLevelCooldownTimer = k_nextLevelCooldown;
     }
 
     [Rpc(SendTo.Server)]
--- Assets/Scripts/Interactables/Hole.cs
+++ Assets/Scripts/Interactables/Hole.cs
@@ -5,7 +5,48 @@ using Unity.Netcode;
 
 
 
-public class Hole : NetworkBehaviour
+public class Hole : Interactable
 {
+    private float m_fHoldTimer = 0;
+    private float k_fHoldtime = 0.5f;
+    private float m_nextLevelCooldownTimer = 0;
+    private float k_nextLevelCooldown = 3;
 
+    public override void OnStartInteraction()
+    {
+        InteractableUICanvas.Instance.InteractTextbox.SetActive(true);
+    }
+
+    public override void OnUpdateInteraction()
+    {
+        m_nextLevelCooldownTimer -= Time.deltaTime;
+        InteractableUICanvas.Instance.InteractSlider.value = m_fHoldTimer / k_fHoldtime;
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            m_fHoldTimer += Time.deltaTime;
+            if (m_fHoldTimer >= k_fHoldtime && m_nextLevelCooldownTimer <= 0)
+            {
+                TryGoToNextLevelServerRpc();
+                m_nextLevelCooldownTimer = k_nextLevelCooldown;
+            }
+        } else
+        {
+            m_fHoldTimer = 0;
+        }
+    }
+
+    public override void OnFinishInteraction()
+    {
+        InteractableUICanvas.Instance.InteractTextbox.SetActive(false);
+    }
+
+    [Rpc(SendTo.Server)]
+    void TryGoToNextLevelServerRpc()
+    {
+        if (m_nextLevelCooldownTimer > 0) return;
+
+        LevelManager.Instance.GoToNextLevel();
+        m_nextLevelCooldownTimer = k_nextLevelCooldown;
+    }
 }
