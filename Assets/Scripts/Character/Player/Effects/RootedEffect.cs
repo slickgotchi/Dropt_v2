9a52b9ea37074becaa25727fb7817f6f33f99d72
Slickgotchi <slickgotchi@gmail.com>
2024-08-06 09:28:48 +0800

    pretrap merge

---
+++ Assets/Scripts/Character/Player/Effects/RootedEffect.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootedEffect : MonoBehaviour
+{
+    private NetworkCharacter m_networkCharacter;
+    private float m_rootedTimer = 0;
+    private BuffObject m_buffObject;
+
+    void Init(float duration, BuffObject buffObject)
+    {
+        m_networkCharacter = GetComponent<NetworkCharacter>();
+        if (m_buffObject != null)
+        {
+            m_networkCharacter.RemoveBuffObject(m_buffObject);
+        }
+
+        m_rootedTimer = duration > 0 ? duration : 0.1f;
+        m_buffObject = buffObject;
+
+        m_networkCharacter.AddBuffObject(m_buffObject);
+    }
+
+    public static void ApplyRootedEffect(GameObject target, float duration, BuffObject rootedBuff)
+    {
+        RootedEffect rootedEffect = target.GetComponent<RootedEffect>();
+        if (rootedEffect == null)
+        {
+            rootedEffect = target.gameObject.AddComponent<RootedEffect>();
+        }
+        rootedEffect.Init(duration, rootedBuff);
+    }
+
+    private void Update()
+    {
+        m_rootedTimer -= Time.deltaTime;
+
+        if (m_rootedTimer <= 0 && m_buffObject != null)
+        {
+            m_networkCharacter.RemoveBuffObject(m_buffObject);
+            m_buffObject = null;
+        }
+    }
+}
