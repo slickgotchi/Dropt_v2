23d54abe7e436713dae40965b482d4105204c839
Slickgotchi <slickgotchi@gmail.com>
2024-08-12 11:39:25 +0800

    try deploy

---
+++ Assets/Scripts/Levels/NetworkLevel_NetworkObjectSpawners.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+// NetworkObjectSpawners
+public partial class NetworkLevel : NetworkBehaviour
+{
+    public void CreateNetworkObjectSpawners()
+    {
+
+    }
+}
