ee368b73e643ac9e964a0cc8c09f34d9167eaf4f
Slickgotchi <slickgotchi@gmail.com>
2024-09-13 10:24:21 +0800

    updated ballistic projectiles

---
+++ Assets/Scripts/AI/EnemyAI_FudSpirit.cs
@@ -0,0 +1,66 @@
+using Dropt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Unity.Mathematics;
+
+namespace Dropt
+{
+    public class EnemyAI_FudSpirit : EnemyAI
+    {
+        private Animator m_animator;
+
+        private void Awake()
+        {
+            m_animator = GetComponent<Animator>();
+        }
+
+        public override void OnSpawnStart()
+        {
+        }
+
+        public override void OnTelegraphStart()
+        {
+            // calc our attack direction
+            CalculateAttackDirection();
+
+            // set our facing direction
+            GetComponent<EnemyController>().SetFacingFromDirection(AttackDirection, TelegraphDuration);
+        }
+        
+        public override void OnRoamUpdate(float dt)
+        {
+            SimpleRoamUpdate(dt);   
+        }
+
+        public override void OnAggroUpdate(float dt)
+        {
+            SimplePursueUpdate(dt);
+        }
+
+        public override void OnAttackStart()
+        {
+            SimpleAttackStart();
+
+            // set facing
+            GetComponent<EnemyController>().SetFacingFromDirection(AttackDirection, AttackDuration);
+        }
+
+        public override void OnCooldownStart()
+        {
+            // teleport to new location here
+        }
+
+        public override void OnCooldownUpdate(float dt)
+        {
+            
+        }
+
+        public override void OnKnockback(Vector3 direction, float distance, float duration)
+        {
+            SimpleKnockback(direction, distance, duration);
+
+        }
+    }
+}
--- Assets/Scripts/Character/Enemies/EnemyController.cs
+++ Assets/Scripts/Character/Enemies/EnemyController.cs
@@ -51,65 +51,10 @@ public class EnemyController : NetworkBehaviour
 
         }
 
-        ApplyDynamicHp();
     }
 
     public override void OnNetworkDespawn()
     {
-    }
-
-    void ApplyDynamicHp()
-    {
-        if (!IsServer) return;
-
-        var playerCharacters = FindObjectsByType<PlayerCharacter>(FindObjectsSortMode.None);
-        var netAttackPowers = new List<float>();
-        foreach (var pc in playerCharacters)
-        {
-            netAttackPowers.Add(GetPlayerNetAttackPower(pc));
-        }
-
-        netAttackPowers.Sort((a, b) => b.CompareTo(a));
-
-        // now assign dynamic HP
-        float dynamicHp = 0;
-        if (netAttackPowers.Count == 1)
-        {
-            dynamicHp = netAttackPowers[0];
-        } else if (netAttackPowers.Count == 2)
-        {
-            dynamicHp = netAttackPowers[0] + netAttackPowers[1] * 0.5f;
-        } else if (netAttackPowers.Count == 3)
-        {
-            dynamicHp = netAttackPowers[0] + netAttackPowers[1] * 0.5f + netAttackPowers[2] * 0.25f;
-        }
-
-        dynamicHp *= DynamicHpMultiplier;
-
-        GetComponent<NetworkCharacter>().HpMax.Value += dynamicHp;
-        GetComponent<NetworkCharacter>().HpCurrent.Value += dynamicHp;
-    }
-
-    float GetPlayerNetAttackPower(PlayerCharacter playerCharacter)
-    {
-        var playerEquipment = playerCharacter.GetComponent<PlayerEquipment>();
-        var lhWearableNameEnum = playerEquipment.LeftHand.Value;
-        var rhWearableNameEnum = playerEquipment.RightHand.Value;
-
-        var lhWearable = WearableManager.Instance.GetWearable(lhWearableNameEnum);
-        var rhWearable = WearableManager.Instance.GetWearable(rhWearableNameEnum);
-
-        float lhRarityMultiplier = lhWearable == null ? 1 : lhWearable.RarityMultiplier;
-        float rhRarityMultiplier = rhWearable == null ? 1 : rhWearable.RarityMultiplier;
-
-        float rarityMultiplier = math.max(lhRarityMultiplier, rhRarityMultiplier);
-
-        var baseAttack = playerCharacter.AttackPower.Value * rarityMultiplier;
-        var baseCrit = playerCharacter.CriticalChance.Value;
-
-        float netAttackPower = (baseAttack * (1 - baseCrit) + (baseAttack * 2 * baseCrit));
-
-        return netAttackPower;
     }
 
     // Update is called once per frame
