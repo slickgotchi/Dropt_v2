6852969a0e8f9fed837e3e7ef225f0cd67eecceb
Slickgotchi <slickgotchi@gmail.com>
2024-09-13 09:04:56 +0800

    bomb snails done

---
+++ Assets/Scripts/Character/Enemies/DynamicHP.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using Unity.Mathematics;
+
+public class DynamicHP : NetworkBehaviour
+{
+    public float Multiplier = 1f;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        ApplyDynamicHp();
+    }
+
+    void ApplyDynamicHp()
+    {
+        if (!IsServer) return;
+
+        var playerCharacters = FindObjectsByType<PlayerCharacter>(FindObjectsSortMode.None);
+        var netAttackPowers = new List<float>();
+        foreach (var pc in playerCharacters)
+        {
+            netAttackPowers.Add(GetPlayerNetAttackPower(pc));
+        }
+
+        netAttackPowers.Sort((a, b) => b.CompareTo(a));
+
+        // now assign dynamic HP
+        float dynamicHp = 0;
+        if (netAttackPowers.Count == 1)
+        {
+            dynamicHp = netAttackPowers[0];
+        }
+        else if (netAttackPowers.Count == 2)
+        {
+            dynamicHp = netAttackPowers[0] + netAttackPowers[1] * 0.5f;
+        }
+        else if (netAttackPowers.Count == 3)
+        {
+            dynamicHp = netAttackPowers[0] + netAttackPowers[1] * 0.5f + netAttackPowers[2] * 0.25f;
+        }
+
+        dynamicHp *= Multiplier;
+
+        GetComponent<NetworkCharacter>().HpMax.Value += dynamicHp;
+        GetComponent<NetworkCharacter>().HpCurrent.Value += dynamicHp;
+    }
+
+    float GetPlayerNetAttackPower(PlayerCharacter playerCharacter)
+    {
+        var playerEquipment = playerCharacter.GetComponent<PlayerEquipment>();
+        var lhWearableNameEnum = playerEquipment.LeftHand.Value;
+        var rhWearableNameEnum = playerEquipment.RightHand.Value;
+
+        var lhWearable = WearableManager.Instance.GetWearable(lhWearableNameEnum);
+        var rhWearable = WearableManager.Instance.GetWearable(rhWearableNameEnum);
+
+        float lhRarityMultiplier = lhWearable == null ? 1 : lhWearable.RarityMultiplier;
+        float rhRarityMultiplier = rhWearable == null ? 1 : rhWearable.RarityMultiplier;
+
+        float rarityMultiplier = math.max(lhRarityMultiplier, rhRarityMultiplier);
+
+        var baseAttack = playerCharacter.AttackPower.Value * rarityMultiplier;
+        var baseCrit = playerCharacter.CriticalChance.Value;
+
+        float netAttackPower = (baseAttack * (1 - baseCrit) + (baseAttack * 2 * baseCrit));
+
+        return netAttackPower;
+    }
+}
