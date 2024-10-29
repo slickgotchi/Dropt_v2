828aa94242ee79e13350dfef69a8af9539655cd2
Krunal Harsoda <65004130+harsodakrunal@users.noreply.github.com>
2024-10-29 17:13:36 +0530

    29 Oct 2024
    - Set animations for  LeafShade & GeodeShade

--- Assets/Scripts/Character/Enemies/AI/EnemyAI_LeafShade.cs
+++ Assets/Scripts/Character/Enemies/AI/EnemyAI_LeafShade.cs
@@ -1,10 +1,4 @@
-using Dropt;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-using Unity.Mathematics;
-using Unity.Netcode;
 
 namespace Dropt
 {
@@ -19,6 +13,7 @@ namespace Dropt
 
         public override void OnSpawnStart()
         {
+            Utils.Anim.PlayAnimationWithDuration(m_animator, "LeafShade_Spawn", SpawnDuration);
         }
 
         public override void OnTelegraphStart()
@@ -26,12 +21,22 @@ namespace Dropt
             if (IsServer)
             {
                 m_navMeshAgent.isStopped = true;
+                Utils.Anim.PlayAnimationWithDuration(m_animator, "LeafShade_Anticipation", TelegraphDuration);
             }
         }
-        
+
+        public override void OnRoamStart()
+        {
+            base.OnRoamStart();
+            if (IsServer)
+            {
+                Utils.Anim.Play(m_animator, "LeafShade_Roam");
+            }
+        }
+
         public override void OnRoamUpdate(float dt)
         {
-            SimpleRoamUpdate(dt);   
+            SimpleRoamUpdate(dt);
         }
 
         public override void OnAggroUpdate(float dt)
@@ -41,9 +46,11 @@ namespace Dropt
 
         public override void OnAttackStart()
         {
+            if (IsServer)
+            {
+                Utils.Anim.Play(m_animator, "LeafShade_Attack");
+            }
             SimpleAttackStart();
-
-            // set facing
             GetComponent<EnemyController>().SetFacingFromDirection(AttackDirection, AttackDuration);
         }
 
