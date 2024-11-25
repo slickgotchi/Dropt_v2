86d138f3249acf00c01778f4acdb2d969aacf43b
Slickgotchi <slickgotchi@gmail.com>
2024-11-25 12:28:58 +0800

    debug done

--- Assets/Scripts/Debugging/NetworkCharacterDebugCanvasToggler.cs
+++ Assets/Scripts/Debugging/NetworkCharacterDebugCanvasToggler.cs
@@ -6,28 +6,28 @@ public class NetworkCharacterDebugCanvasToggler : MonoBehaviour
 {
     private bool m_isVisible = false;
 
-    private void Start()
-    {
-        SetVisibile(false);
-    }
+    //private void Start()
+    //{
+    //    SetVisibile(false);
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SetVisibile(!m_isVisible);
-        }
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Alpha2))
+    //    {
+    //        SetVisibile(!m_isVisible);
+    //    }
+    //}
 
-    void SetVisibile(bool visible)
-    {
-        var canvii = FindObjectsByType<NetworkCharacterDebugCanvas>(FindObjectsSortMode.None);
-        foreach (var canvas in canvii)
-        {
-            canvas.Container.SetActive(visible);
-        }
+    //void SetVisibile(bool visible)
+    //{
+    //    var canvii = FindObjectsByType<NetworkCharacterDebugCanvas>(FindObjectsSortMode.None);
+    //    foreach (var canvas in canvii)
+    //    {
+    //        canvas.Container.SetActive(visible);
+    //    }
 
-        m_isVisible = visible;
-    }
+    //    m_isVisible = visible;
+    //}
 }
