96228229e52fb5f76c1004a317ecf481005a37e1
Krunal Harsoda <65004130+harsodakrunal@users.noreply.github.com>
2024-12-09 13:21:23 +0530

    9 Dec 2024
    - Update Spider and SpiderPod Spritesheet Animations

--- Assets/Scripts/Utility/AnimUtils.cs
+++ Assets/Scripts/Utility/AnimUtils.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Dropt.Utils
 {
-
     public static class Anim
     {
         public static void PlayAnimationWithDuration(Animator animator, string animName, float duration)
